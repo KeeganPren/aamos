475dce02fb135527e9775d80dfc6899c307a88d3
Keegan Prendergast <keegan.prendergast@gmail.com>
2025-02-15 18:55:00 -0600

    Added a basic build script, and copyright statements to CMakeLists.txt files.

---
+++ build.sh
@@ -0,0 +1,20 @@
+#
+# Copyright (c) 2025 Keegan Prendergast
+# 
+
+# Generate the build files
+cmake -S . -B build
+
+# Generate the build
+cmake --build build
+
+# Run the tests
+cd build; ctest -T Test -T Coverage; cd ..
+
+# Generate and cleanup to coverage data
+lcov --directory build --capture --output-file coverage.info
+lcov --remove coverage.info '/usr/*' --output-file coverage.info
+genhtml -o coverage coverage.info
+
+# Generate documentation
+cd src; make docs; cd ..
